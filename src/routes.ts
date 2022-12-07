0e26e54edb84334f30f4ca7ef84238d95c8a45e6
Zapplightforce <pers0010@hz.nl>
2022-12-07 15:09:48 +0100

    created database structure

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import {Express} from "express";
 import {logger} from "./middleware/logger";
+import {creation} from "./routes/creation";
 
 export const routes = (app: Express) => {
+    app.use(creation);
     app.get("/", (req, res) => res.send('Hello World'));
     app.get("/good", logger, (req, res) => res.status(200).json({success: 'Well done this route is working perfectly'}))
     app.get("/bad", (req, res) => res.status(500).json({error: 'Too bad this route does mean something does not work correctly'}))
