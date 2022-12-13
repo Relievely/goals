f2e6ff428bde29de5f647356b6ab637842073ae8
Marcel Dominic Bandowski <marcel.bandowski@dextermedia.net>
2022-12-13 10:07:40 +0100

    Added function to insert a new reminder

--- src/routes/goals.test.ts
+++ src/routes/goals.test.ts
@@ -3,12 +3,26 @@ import { databaseInit } from "./jestPresets";
 import supertest, {Response} from "supertest";
 import {app} from "../app";
 import {GoalItem, ResponseObject} from "../interfaces";
+import {RunResult} from "better-sqlite3";
 
 beforeAll(() => databaseInit());
 
 const requestWithSuperTest = supertest(app);
 
 describe("Goal routes", () => {
+    it("should insert a new reminder", async () => {
+        await requestWithSuperTest
+            .post("/goals/reminder")
+            .send({name: "Bake 20 cookies", triggerTime: 1670920679, referenceID: 1})
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then((response: Response) => {
+                expect(response).toBeDefined();
+                const responseObject = response.body as ResponseObject<RunResult>
+                expect(responseObject.data.length).toBeGreaterThanOrEqual(1);
+            });
+    })
+
     it("should get all goal items", async () => {
         await requestWithSuperTest
             .get("/goals")
